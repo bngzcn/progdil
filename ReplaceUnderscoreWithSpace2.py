e6496251393bf99866e438b691964168ea221007
Bengü Özcan <bengu.ozcan@bil.omu.edu.tr>
2014-10-14 17:55:30 +0300

    Create ReplaceUnderscoreWithSpace2.py

---
+++ ReplaceUnderscoreWithSpace2.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+import sys
+
+class StringUtils(object):
+	def __init__(self):
+		pass
+
+	def ReplaceUnderscoreWithSpace(self, string):
+		length = len(string)
+		if(length == 1):
+			return string
+		listOfString  = list(string)
+		i = 1
+		while(i < length -1 ):
+			if(listOfString[i] == "_"):
+				listOfString[i] = " "
+			i += 1
+		return  ''.join(listOfString)
+
+def main():
+	stringUtils = StringUtils()
+	for arg in sys.argv[1:]:
+		print stringUtils.ReplaceUnderscoreWithSpace(arg)
+
+if __name__ == '__main__':
+	main()

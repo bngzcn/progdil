484bd0063031b4d2e2f798c83102adcea2b54940
Bengü Özcan <bengu.ozcan@bil.omu.edu.tr>
2014-10-17 23:51:27 +0300

    Update ReplaceUnderscoreWithSpace.py

--- ReplaceUnderscoreWithSpace.py
+++ ReplaceUnderscoreWithSpace.py
@@ -2,12 +2,12 @@
 
 def ReplaceUnderscoreWithSpace(string):
 	length = len(string)
-	if(length == 1):
+	if (length == 1):
 		return string
 	listOfString  = list(string)
 	i = 1
-	while(i < length -1 ):
-		if(listOfString[i] == "_"):
+	while (i < length -1 ):
+		if (listOfString[i] == "_"):
 			listOfString[i] = " "
 		i += 1
 	return  ''.join(listOfString)
